7239d5f43e30d80bbebdb21e70a32330b5564dc7
Lilian <58983575+Lilian-DL@users.noreply.github.com>
2022-03-15 16:10:35 +0100

    commit

---
+++ src/app/student/pages/student-list/student-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from 'src/app/core/models/student';
+import { StudentService } from '../../services/student.service';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.scss']
+})
+export class StudentListComponent implements OnInit {
+  students$: Observable<Student[]>;
+
+  constructor(private _studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.students$ = this._studentService.get();
+  }
+
+}
